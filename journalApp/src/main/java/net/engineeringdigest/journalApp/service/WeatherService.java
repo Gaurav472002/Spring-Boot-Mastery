b2b4fe52d5e9e520a6baff48c1e04af95c8d1907
Gaurav472002 <sagarchakraborty472@gmail.com>
2026-08-13 01:50:55 +0530

    Implemented mechanism to consume POST request using restTemplate

--- journalApp/src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/service/WeatherService.java
@@ -1,11 +1,15 @@
 package net.engineeringdigest.journalApp.service;
 
 import net.engineeringdigest.journalApp.api.response.WeatherResponse;
+import net.engineeringdigest.journalApp.entity.PostRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestTemplate;import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
 
 @Component
 public class WeatherService {
@@ -38,6 +42,44 @@ public class WeatherService {
                 + body.getCurrent().getWeatherDescription().get(0)
                 + ", Feels like "
                 + body.getCurrent().getFeelslike() + "°C";
+    }
+
+    // POST request using RestTemplate
+    public String sendPost(PostRequest postRequest) {
+
+        String url =
+                "https://jsonplaceholder.typicode.com/posts";
+
+
+        // 1. Create headers
+        HttpHeaders headers =
+                new HttpHeaders();
+
+        headers.setContentType(
+                MediaType.APPLICATION_JSON
+        );
+
+
+        // 2. Combine body + headers
+        HttpEntity<PostRequest> requestEntity =
+                new HttpEntity<>(
+                        postRequest,
+                        headers
+                );
+
+
+        // 3. Send POST request
+        ResponseEntity<String> response =
+                restTemplate.exchange(
+                        url,
+                        HttpMethod.POST,
+                        requestEntity,
+                        String.class
+                );
+
+
+        // 4. Get response body
+        return response.getBody();
     }
 
 
